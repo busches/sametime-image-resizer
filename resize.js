0bc9a797feafb20b3ecc7d1909c1e3ffffaaf6c9
Scott Busche <swbusche@gmail.com>
2015-06-22 15:32:51 -0500

    Add .jscsrc file and fix the errors

--- resize.js
+++ resize.js
@@ -8,10 +8,10 @@ var fs = require('fs');
 var args = process.argv.slice(2);
 
 if (args.length === 0) {
-  console.log("No image supplied to resize!");
+  console.log('No image supplied to resize!');
   return;
 } else if (args.length > 1) {
-  console.log("Too many arguments, only expecting 1, received " + args.length);
+  console.log('Too many arguments, only expecting 1, received ' + args.length);
 }
 
 var input = args[0];
@@ -20,16 +20,21 @@ var tempOutput = tempfile('.gif');
 
 var debug = true;
 
-execFile(gifsiclePath, [input, '--size-info'], function(err, stdout) {
+execFile(gifsiclePath, [input, '--size-info'], function processFileSize(err, stdout) {
   if (err) {
     return cb(err);
   }
 
   var dimensions;
+
+  var parseDimensions = function(stdIn) {
+    return stdIn.replace('logical screen', '').trim().split('x');
+  };
+
   var parsedStdOut = stdout.split('\r\n');
   for (var lineNum = parsedStdOut.length - 1; lineNum >= 0; lineNum--) {
-    if (parsedStdOut[lineNum].indexOf("logical screen") > 0) {
-      dimensions = parsedStdOut[lineNum].replace("logical screen", "").trim().split("x");
+    if (parsedStdOut[lineNum].indexOf('logical screen') > 0) {
+      dimensions = parseDimensions(parsedStdOut[lineNum]);
       if (debug) {
         console.log(dimensions);
       }
@@ -49,51 +54,55 @@ execFile(gifsiclePath, [input, '--size-info'], function(err, stdout) {
   var newWidth = originalWidth;
   var idealSize = 512000;
 
-  // newWidth -= 100;
+  var buildArgs = function(width, outputFileName, input) {
+    return ['--resize-width', width, '-o', outputFileName, input, '-O3'];
+  };
 
-  var resize = function() {
-    execFile(gifsiclePath, ['--resize-width', newWidth, '-o', tempOutput, input, '-O3'], function(err) {
+  var validateSize = function(err) {
+    if (err) {
+      return cb(err);
+    }
+
+    fs.stat(tempOutput, function(err, stat) {
       if (err) {
         return cb(err);
       }
+      size = stat.size;
+      if (debug) {
+        console.log('Size is now: ' + size + ' - ' + newWidth);
+      }
 
-      fs.stat(tempOutput, function(err, stat) {
-        if (err) {
-          return cb(err);
-        }
-        size = stat.size;
-        if (debug) {
-          console.log('Size is now: ' + size + ' - ' + newWidth);
-        }
-
-        if (size > idealSize) {
-          if (size > 1024000) {
-            newWidth -= 50;
-          } else if (size > 768000) {
-            newWidth -= 10;
-          } else if (size > 563200) {
-            newWidth -= 5;
-          } else {
-            newWidth -= 1;
-          }
-          resize();
+      if (size > idealSize) {
+        if (size > 1024000) {
+          newWidth -= 50;
+        } else if (size > 768000) {
+          newWidth -= 10;
+        } else if (size > 563200) {
+          newWidth -= 5;
         } else {
-          copyFile(tempOutput, output, function(err) {
-            if (err) {
-              return cb(err);
-            }
-            console.log("All done");
-          });
+          newWidth -= 1;
         }
-      });
+        resize();
+      } else {
+        copyFile(tempOutput, output, function(err) {
+          if (err) {
+            return cb(err);
+          }
+          console.log('All done');
+        });
+      }
     });
+  };
+
+  var resize = function() {
+    execFile(gifsiclePath, buildArgs(newWidth, tempOutput, input), validateSize);
   };
 
   resize();
 });
 
 function cb(err) {
-  console.log("shit's broken");
+  console.log('Something broke');
   console.log(err);
 }
 
@@ -101,14 +110,14 @@ function copyFile(source, target, cb) {
   var cbCalled = false;
 
   var rd = fs.createReadStream(source);
-  rd.on("error", function(err) {
+  rd.on('error', function(err) {
     done(err);
   });
   var wr = fs.createWriteStream(target);
-  wr.on("error", function(err) {
+  wr.on('error', function(err) {
     done(err);
   });
-  wr.on("close", function() {
+  wr.on('close', function() {
     done();
   });
   rd.pipe(wr);
