5d6b724b1eac17df4f7a3f3d274b5150c43df995
Scott Busche <swbusche@gmail.com>
2018-10-18 10:12:15 -0500

    Clean up the code with some ES6

--- resize.js
+++ resize.js
@@ -17,65 +17,62 @@ const args = process.argv.slice(2);
 if (args.length === 0) {
   throw new Error('No image supplied to resize!');
 } else if (args.length > 1) {
-  throw new Error('Too many arguments, only expecting 1, received ' + args.length);
+  throw new Error(`Too many arguments, only expecting 1, received ${args.length}`);
 }
 
 const input = args[0];
 const inputPath = path.parse(input);
-let output = 'shrunk_' + inputPath.base;
+let output = `shrunk_${inputPath.base}`;
 if (inputPath.dir.length > 0) {
-  output = inputPath.dir + '\\' + output;
+  output = `${inputPath.dir}\\${output}`;
 }
 const tempOutput = tempfile('.gif');
 const debug = true;
 
-const handleError = err => print.error('Something has gone wrong:\n' + err);
+const handleError = err => print.error(`Something has gone wrong:
+${err}`);
 
 const parseDimensionsFromOutput = stdIn => stdIn.replace('logical screen', '').trim().split('x');
 
-const parseDimensions = function (stdout) {
-  return stdout[0].split('\r\n')
-    .filter(line => line.indexOf('logical screen') > 0)
-    .map(rawDimensions => {
-      const dimensions = parseDimensionsFromOutput(rawDimensions);
-      if (debug) {
-        print.info('Starting size - ' + dimensions[0] + 'x' + dimensions[1]);
-      }
-      return dimensions[0];
-    })
-    .reduce((dimensions, dimension, i) => {
-      dimensions[i === 0 ? 'x' : 'y'] = dimension;
-      return dimensions;
-    }, {});
-};
+const parseDimensions = stdout => stdout[0].split('\r\n')
+  .filter(line => line.indexOf('logical screen') > 0)
+  .map(rawDimensions => {
+    const dimensions = parseDimensionsFromOutput(rawDimensions);
+    if (debug) {
+      print.info(`Starting size - ${dimensions[0]}x${dimensions[1]}`);
+    }
+    return dimensions[0];
+  })
+  .reduce((dimensions, dimension, i) => {
+    dimensions[i === 0 ? 'x' : 'y'] = dimension;
+    return dimensions;
+  }, {});
 
 const buildArgs = (width, outputFileName, input) => ['--resize-width', width, '-o', outputFileName, input, '-O3'];
 
-const copySuccessful = function () {
+const copySuccessful = () => {
   const endTime = process.hrtime(startTime);
-  print.success('All done - ' + prettyHrtime(endTime));
+  print.success(`All done - ${prettyHrtime(endTime)}`);
 };
 
-const copyFile = function (source, target) {
-  return fs.readFile(source)
-    .then(fs.writeFile.bind(fs, target))
-    .then(copySuccessful)
-    .catch(handleError);
-};
+const copyFile = (source, target) => fs.readFile(source)
+  .then(fs.writeFile.bind(fs, target))
+  .then(copySuccessful)
+  .catch(handleError);
 
 const getStartingWidth = () => execFile(gifsicle, [input, '--size-info']).then(data => parseDimensions(data).x);
 
 const getFileSize = file => fs.stat(file);
 
 // Should be able to split this out further
-const resize = function (width, inputFile, initialRun) {
+const resize = (width, inputFile, initialRun) => {
   const idealSize = 5 * 1024 * 1024;
   let newWidth = width;
 
   getFileSize(inputFile).then(fileData => {
     const {size} = fileData;
     if (debug) {
-      print.info('Size is now: ' + size + ' - ' + newWidth);
+      print.info(`Size is now: ${size} - ${newWidth}`);
     }
 
     if (size > idealSize) {
@@ -98,7 +95,7 @@ const resize = function (width, inputFile, initialRun) {
     } else if (initialRun) {
       print.info('File doesn\'t need resizing!');
       if (input !== output) {
-      copyFile(input, output);
+        copyFile(input, output);
       }
     } else {
       copyFile(tempOutput, output);
